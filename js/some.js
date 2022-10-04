9ad35921fdc59db98e2ad381d64ef6a8568df486
Mauricio Fernandez <mfernandezuk@hotmail.com>
2022-10-04 11:43:16 -0300

    correction in the code links

--- js/some.js
+++ js/some.js
@@ -1,8 +1,19 @@
 let search = document.querySelector('.search');
-search.addEventListener('click',()=>{
-    let input = document.querySelector('#search-value').value;
-    let exists = Courses.some(element => element.name.toLowerCase() === input.toLowerCase());
+const searchInput = document.querySelector('#search-value')
+
+const checkCourse = () =>
+{
+    const input = searchInput.value;
+    const exists = Courses.some(element => element.name.toLowerCase() === input.toLowerCase());
     const label = document.querySelector('#result');
     const message = exists ? 'True' : 'False';
     label.innerText = message;
-})
+}
+search.addEventListener('click', checkCourse);
+searchInput.addEventListener("keypress", (e)=> {
+
+    if (e.key === "Enter")
+    {
+        checkCourse();
+    }
+});
